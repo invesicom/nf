472451f937f9c1ea984ee3e5d74fa804f186ac98
SDH <admin@stardothosting.com>
2025-07-16 23:46:32 -0400

    removed redundant script

--- pull_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/bin/php82 /usr/local/bin/composer dump-autoload;/usr/bin/php82 artisan config:clear;/usr/bin/php82 artisan cache:clear;/usr/bin/php82 artisan route:clear;/usr/bin/php82 artisan view:clear;rm -rf bootstrap/cache/*;systemctl restart php82-php-fpm;chown -R nullfake:nginx *;systemctl restart nullfake-queue
