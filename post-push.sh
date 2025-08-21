4a4e979e2b1166fd2e321a908566da987272d6d2
SDH <admin@stardothosting.com>
2025-08-21 15:29:30 -0400

    Updated post push script

--- post-push.sh
+++ post-push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/php82 /usr/local/bin/composer dump-autoload;php82 artisan config:clear;php82 artisan cache:clear;php82 artisan route:clear;php82 artisan view:clear;rm -rf bootstrap/cache/*;systemctl restart php82-php-fpm;chown -R nullfake:nginx *;systemctl restart supervisord;php82 artisan livewire:publish 
+/usr/bin/php82 /usr/local/bin/composer dump-autoload;php82 artisan config:clear;php82 artisan cache:clear;php82 artisan route:clear;php82 artisan view:clear;rm -rf bootstrap/cache/*;systemctl restart php82-php-fpm;systemctl restart supervisord;php82 artisan livewire:publish;chown -R nullfake:nginx *
 
